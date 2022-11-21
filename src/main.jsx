916aaff01d5ade302a6ae7b693eea03588b5b166
belino93 <abelmadrid93@gmail.com>
2022-11-21 18:07:55 +0100

    Create folder frame

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {BrowserRouter, Router, Route, Routes} from 'react-router-dom'
+import Header from './components/Header/Header'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
   <React.StrictMode>
-    <App />
+    
+  <Header/>
+    <Routes>
+
+      <Route path='/' element={<App />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+
+    </Routes>
+
+    
   </React.StrictMode>
+  </BrowserRouter>
 )
