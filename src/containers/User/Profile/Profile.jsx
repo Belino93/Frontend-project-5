c4194e974d1f685f6f8b84ec98939a2d0b0e642f
belino93 <abelmadrid93@gmail.com>
2022-12-01 09:31:22 +0100

    Add some functions in profile

--- src/containers/User/Profile/Profile.jsx
+++ src/containers/User/Profile/Profile.jsx
@@ -6,6 +6,9 @@ import {
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Profile.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Profile() {
   const imgUrl = "https://image.tmdb.org/t/p/w200/";
@@ -15,7 +18,7 @@ function Profile() {
     navigate("/login");
   }
 
-  const [refund, setRefund] = useState(false)
+  const [refund, setRefund] = useState(false);
   const [profile, setProfile] = useState({
     name: "",
     surname: "",
@@ -47,35 +50,41 @@ function Profile() {
   const clickHandler = (lease_id) => {
     updateLease(tokenjw, lease_id).then((res) => {
       console.log(res);
-      setRefund(!refund)
+      setRefund(!refund);
     });
-    
   };
 
   useEffect(() => {
     getLeaseById(tokenjw).then((leaseData) => {
       setLeases(leaseData.data);
     });
-  }, [refund])
-  
+  }, [refund]);
 
   if (leases.length > 0) {
     return (
-      <div className="container-profile">
-        <div className="profile-card">
-          <h1>Profile</h1>
-          <h3>
-            Hello, {profile.name} {profile.surname}
-          </h3>
-          <p>{profile.email}</p>
-          <p>ACTIVE RENTALS</p>
+      <Container fluid className="container-profile">
+        <Row>
+          <div className="profile-card">
+            <h3>Your profile, {profile.name}</h3>
+            <p>{profile.email}</p>
+          </div>
+        </Row>
+
+        <Row>
+          <p>YOUR ACTIVE RENTALS</p>
           <div className="container-rental-active">
             {leases.map((lease, index) => (
               <div className="rental-active" key={index}>
-                <img src={imgUrl + lease.Movies[0].poster} />
-                <p>{lease.Movies[0].title}</p>
+                <img
+                  className="rental-img"
+                  src={imgUrl + lease.Movies[0].poster}
+                />
+                <div className="rental-title">
+                  {lease.Movies[0].title.slice(0, 20) + "..."}
+                </div>
                 <p>Rental date: {lease.createdAt}</p>
                 <button
+                  className="rental-button"
                   onClick={() => {
                     clickHandler(lease.lease_id);
                   }}
@@ -85,8 +94,8 @@ function Profile() {
               </div>
             ))}
           </div>
-        </div>
-      </div>
+        </Row>
+      </Container>
     );
   }
   return (
