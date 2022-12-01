6543dcb1943ecce5d56a71467bad53df70b17edd
LizaCastDiaz <lizacastdiaz@gmail.com>
2022-12-01 18:11:29 +0100

    Everything is runnning

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import {useJwt} from 'react-jwt'
 
 function Header() {
   const navigate = useNavigate();
--- src/containers/Films/FilmsCards.jsx
+++ src/containers/Films/FilmsCards.jsx
@@ -6,7 +6,7 @@ const FilmsCards = () => {
   const [movies, setMovies] = useState([])
   const bringMovies = async () => {
 
-    const movies = await axios.get("http://localhost:3005/movie")
+    const movies = await axios.get("http://localhost:3006/movie")
     return movies
   }
   useEffect(() => {
@@ -33,28 +33,15 @@ const FilmsCards = () => {
           
             <h3 className="" src="">{movie.title}</h3>
 
-
-const FilmsCards = ({ movies }) => {
-  return (
-    <div className="container">
-      {/* <div className="row">
-        {movies.map((item, index) => (
-          <div key={index} className="col-lg-3 col-md-6 col-sm-12 mb-4">
-            <div className="card" style={{ minWidth: "200px" }}>
-              <img className="card-img-top" src={item.poster} alt="poster" />
-              <div className="card-body">
-                <h5 className="card-title">Title{item.title}</h5>
-                <hr />
-                <p className="card-text">Genre: {item.genre}</p>
-                <p className="card-text">ID movie: {item.movie_id.movies}</p>
-              </div>
-            </div>
           </div>
-        ))}
-      </div> */}
-    </div>
-  );
+)        })}
+      </div>
+    );
 
+  }
+return (
+  <div><h1>pelis</h1></div>
+)
 };
 
 export default FilmsCards;
